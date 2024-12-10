d328eafab3e1d806ff9a7ceeaa9f1ab53aa26e58
Felix Dorner <felix.dorner@vorwerk.de>
2024-12-10 21:58:23 +0100

    day6 part1 and part2

--- src/day6.clj
+++ src/day6.clj
@@ -1,9 +1,9 @@
-(ns day4
+(ns day6
   (:require
    [clojure.string :as s]
    [clojure.java.io :as io]))
 
-(defn guard-path [rectangle]
+(defn guard-path [extra-obstacle rectangle]
   (let [width  (count (first rectangle))
         height (count rectangle)
         data   (s/join rectangle)
@@ -15,28 +15,51 @@
                   (+ % width)
                   -1
                   )
-        l      #(if (= 0 (mod % width))           ;; make sure to not jump back to
-                  -1                              ;; the end of the previous line
-                  (dec %))                        ;; if at the beginning of a line
+        l      #(if (= 0 (mod % width)) ;; make sure to not jump back to
+                  -1                    ;; the end of the previous line
+                  (dec %))              ;; if at the beginning of a line
         r      #(if (= (dec width) (mod % width)) ;; make sure to not jump to the 
                   -1                              ;; beginning of the next line
                   (inc %))                        ;; if at the end of a line
-        turn   {up r r dn dn l l up}
-        obstacle? (fn [pos] (= \# (nth data pos)))]
-    (loop [path (list start)
-           dir up]
-      (let [p (first path)            
-            newpos (dir p)]
+        turn   #(get {up r r dn dn l l up} %)
+        obstacle? (fn [pos]
+                    (or (= extra-obstacle pos)
+                        (= \# (nth data pos))))]
+    (loop [[p d] [start up]
+           visited #{[p d]}]
+      (let [newpos (d p)]
         (if (= -1 newpos)
-          path
+          [visited false]
           (if (obstacle? newpos)
-            (recur path (get turn dir))
-            (recur (conj path newpos) dir)))))))
+            (recur [p (turn d)] visited)
+            (if (contains? visited [newpos d])
+              [visited true]
+              (recur [newpos d] (conj visited [newpos d])))))))))
+
 
 (comment "Part 1"
   (with-open [r (-> "day6.txt"
                     io/resource
                     io/reader)]
-    (count (into #{} (guard-path (line-seq r))))))
+    (->> r line-seq (guard-path nil) first (map first) (into #{}) count)))
+
+(comment "Part 2"
+  (with-open [r (-> "day6.txt"
+                    io/resource
+                    io/reader)]
+    (let [rectangle (line-seq r)]
+          (->> rectangle
+               (guard-path nil)
+               (first)
+               (map first)
+               (into #{})
+               (filter (fn [extra-obstacle]
+                         (->> rectangle
+                              (guard-path extra-obstacle)
+                              second)))
+               count)))
 )
-    
+          
+
+      
+
